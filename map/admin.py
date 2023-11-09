309b0c31d3425c20ebc6803ff3b6bb21c3f0ee0c
KTC96 <kyleclow96@gmail.com>
2023-11-09 13:53:19 +0000

    Add admin functionality

--- map/admin.py
+++ map/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import Event
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('event_date', 'event_location')
+    list_display = ('title','event_date', 'event_location')
+    search_fields = ('title','event_location')
+    summernote_fields = ('event_details')
+    
--- map/models.py
+++ map/models.py
@@ -1,1 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Event(models.Model):
+    title = models.CharField(max_length=250, unique=True, default="test")
+    slug = models.SlugField(max_length=200, unique=True)
+    event_genre = models.CharField(max_length=50)
+    event_price = models.DecimalField(max_digits=10, decimal_places=2)
+    event_location = models.CharField(max_length=250)
+    event_date = models.DateTimeField(default=timezone.now)
+    event_details = models.TextField()
+    saved_by = models.ManyToManyField(User, related_name="event_saved", blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    def number_saved_event(self):
+        return self.saved_by.count()
+
+class SavedEvent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='saved_events')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
+
+
+class Calendar(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    saved_events = models.ManyToManyField(Event, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Calendar"
+
+
+class EventComment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    saved_event = models.ForeignKey(SavedEvent, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    hotel_details = models.TextField(blank=True, null=True)
+    transport_details = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
