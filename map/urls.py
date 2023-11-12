79c2911fdea2650018867e24210f676c36af4718
KTC96 <kyleclow96@gmail.com>
2023-11-12 18:23:35 +0000

    add saved events view

--- map/urls.py
+++ map/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home.as_view(), name='home'),
-    path('events_list/', views.EventList.as_view(), name="events")
+    path('events_list/', views.EventList.as_view(), name="events"),
+    path('saved_events/', views.SavedEventList.as_view(), name="saved"),
 ]
--- map/views.py
+++ map/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from django.views import generic
-from .models import Event
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from .models import Event, SavedEvent
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from datetime import date
-
-
+from django.http import HttpResponseRedirect
+from django.core.serializers import serialize
+from django.http import JsonResponse
 import requests
 
 
@@ -17,3 +18,23 @@ class EventList(generic.ListView):
     queryset = Event.objects.filter(event_date__gte=date.today()).order_by('-event_date')
     template_name = 'events_list.html'
     paginate_by = 10
+
+
+
+class SavedEventList(generic.ListView):
+   model = SavedEvent
+   template_name = 'saved_events.html'
+   paginate_by = 5
+
+class SavedEvent(View):
+
+    def event(self, request, slug):
+        event = get_object_or_404(Event, slug=slug)
+
+        if event.saved_by.filter(id=request.user.id).exists():
+            event.saved_by.remove(request.user)
+        else:
+            event.saved_by.add(request.user)
+
+        return HttpResponseRedirect(reverse('home', args=[slug]))
+
