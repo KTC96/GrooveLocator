e8cc508075998cab3c99b772cae4c0ae70ae730d
KTC96 <kyleclow96@gmail.com>
2023-11-15 17:50:59 +0000

    Fix bug that caused no reversematch error

--- map/urls.py
+++ map/urls.py
@@ -1,11 +1,10 @@
 from . import views
 from django.urls import path
 
-
 urlpatterns = [
     path('', views.home.as_view(), name='home'),
-    path('events_list/', views.EventList.as_view(), name="events"),
-    path('saved_events/', views.SavedEventList.as_view(), name="saved"),
+    path('events_list/', views.EventList.as_view(), name="events_list"),
+    path('saved_events/', views.SavedEventList.as_view(), name="saved_events"),
     path('<slug:slug>/', views.EventDetails.as_view(), name="event_details"),
     path('save_event/<slug:slug>/', views.SaveEvent.as_view(), name='save_event'),
 
