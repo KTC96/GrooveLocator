ff5e89f8e1a877c4454c971ee820140da73186d6
KTC96 <kyleclow96@gmail.com>
2023-11-18 13:46:38 +0000

    Add save comment functionality using EventComment model

--- map/views.py
+++ map/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, reverse, render
 from django.views import generic, View
-from .models import Event, SavedEvent
+from .models import Event, SavedEvent, EventComment
 from django.views.generic import TemplateView
 from datetime import date
 from django.http import HttpResponseRedirect, JsonResponse
@@ -26,24 +26,66 @@ class EventList(generic.ListView):
 
 
 class EventDetails(View):
+    template_name = "event_details.html"
+
     def get(self, request, slug, *args, **kwargs):
         event = get_object_or_404(Event, slug=slug)
         details = event.event_details
         saved = False
-        if event.saved.filter(id=self.request.user.id).exists():
-            saved = True
+        user_info = None
+
+        if request.user.is_authenticated:
+            saved = event.saved.filter(id=request.user.id).exists()
+            user_info = EventComment.objects.filter(saved_event__event=event, user=request.user).first()
+
+        comments = EventComment.objects.filter(saved_event__event=event)
 
         return render(
             request,
-            "event_details.html",
+            self.template_name,
             {
                 "event": event,
                 "event_details": details,
                 "saved": saved,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm(instance=user_info),
+                "comments": comments,  # Pass comments to the template
             },
         )
 
+    def post(self, request, slug, *args, **kwargs):
+        event = get_object_or_404(Event, slug=slug)
+        saved = False
+        user_info = None
+
+        if request.user.is_authenticated:
+            saved = event.saved.filter(id=request.user.id).exists()
+            user_info = EventComment.objects.filter(saved_event__event=event, user=request.user).first()
+
+        comment_form = CommentForm(data=request.POST, instance=user_info)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.saved_event, _ = SavedEvent.objects.get_or_create(user=request.user, event=event)
+            comment.save()
+
+            # Redirect to the same page to handle a GET request
+            return HttpResponseRedirect(reverse('event_details', args=[slug]))
+
+        # If the form is not valid, retrieve comments for the current event
+        comments = EventComment.objects.filter(saved_event__event=event)
+
+        return render(
+            request,
+            self.template_name,
+            {
+                "event": event,
+                "event_details": event.event_details,
+                "saved": saved,
+                "comment_form": comment_form,
+                "comments": comments,  # Pass comments to the template
+            },
+        )
 
 class SavedEventList(generic.ListView):
     model = Event
