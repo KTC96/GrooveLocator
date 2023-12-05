a155958c53510030b049cb0b9e91a37f8ad7663d
KTC96 <kyleclow96@gmail.com>
2023-12-05 20:24:00 +0000

    add favicon

--- groove_locator/settings.py
+++ groove_locator/settings.py
@@ -42,7 +42,7 @@ ALLOWED_HOSTS = ['8000-ktc96-groovelocator-w32qxjvnay8.ws-eu106.gitpod.io', 'htt
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # os.environ["DEBUG"]= 'DEVELOPMENT' 
-DEBUG = True
+DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
